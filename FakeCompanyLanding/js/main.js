8ffb85e6e0c4f3f193261a85ae5016fe5f3b114c
Orien Zhang <33295091+zhangori000@users.noreply.github.com>
2020-02-01 21:05:28 -0600

    Add files via upload
    Removed javascript map due to API leak.

---
+++ FakeCompanyLanding/js/main.js
@@ -0,0 +1,38 @@
+function initMap() {
+    //your location
+    const loc = { lat: 42.361145, lng: -71.057083 };
+    //Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map')
+    , {
+        zoom: 14,
+        center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({position:loc, map: map});
+}
+
+//sticky menu backgroudn 
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    }
+    else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+//Smooth scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if(this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800 //speed ms
+        );
+    }
+});
